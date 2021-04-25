ac28214e20f369d7c240dc9d34e28c2472f07beb
Kaleb Ball <kaleb.ball@gmail.com>
2021-04-25 13:20:17 -0500

    Add ci/cd support through Travis CI

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+AWS_S3_REGION="us-east-1"
+PRODUCTION_BRANCH="main"
+
+NODE_ENV=''
+CLOUDFRONT_DIST_ID=''
+if [[ $TRAVIS_BRANCH == $PRODUCTION_BRANCH ]]; then
+  NODE_ENV="prod"
+  CLOUDFRONT_DIST_ID=$CLOUDFRONT_DIST_ID_PRODUCTION
+  yarn build
+else
+  echo "Not deploying"
+  exit
+fi
+
+S3_BUCKET="agora-ui-$NODE_ENV"
+echo "Deploying to the $S3_BUCKET bucket"
+
+pip install awscli --upgrade --user
+
+aws s3 sync public/ "s3://$S3_BUCKET" --acl public-read --delete
+
+aws cloudfront create-invalidation \
+  --distribution-id $CLOUDFRONT_DIST_ID \
+  --paths /*
